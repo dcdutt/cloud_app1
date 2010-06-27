83156126de99a2b674e0503d4b0e553e24c9ba25
Deepak <dcdutt@gmail.com>
2010-06-27 10:04:05 -0400

    Done with coding/testing static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_tag("title",
-                            "Ruby on Rails Tutorial Sample App | Home")
+                            "Home")
     end
 
   end
@@ -26,7 +26,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_tag("title",
-                         "Ruby on Rails Tutorial Sample App | Contact")
+                         "Contact")
     end
 
   end
@@ -40,7 +40,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+                               "About")
     end
 
   end
